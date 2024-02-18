99bea42a66cd4133efb52b261ec052d98fdd27ca
Alejandro <alejandro27campoverde@gmail.com>
2024-02-17 20:40:49 -0500

    RegistrarCita4

--- src/RegistrarCita.java
+++ src/RegistrarCita.java
@@ -1,7 +1,20 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class RegistrarCita {
     public JPanel RegistrarCitaPanel;
+    private JLabel LogoRegistro;
+    private JButton cerrarSesiónButton;
+    private JLabel NombreUsuario;
+
+    public RegistrarCita() {
 
 
+        ImageIcon imageIcon = new ImageIcon("src/MediCare_Logo.png"); /*XD*/
+        Image img = imageIcon.getImage();
+        Image tamaño = img.getScaledInstance(200, 50, Image.SCALE_SMOOTH);
+        ImageIcon imagen = new ImageIcon(tamaño);
+
+        LogoRegistro.setIcon(imagen);
+    }
 }
