ebf5410ec097343cc866b16a429c203f31751df7
Alejandro <alejandro27campoverde@gmail.com>
2024-02-17 19:40:04 -0500

    RegistroCita2

--- src/RegistrarCita.java
+++ src/RegistrarCita.java
@@ -5,4 +5,21 @@ public class RegistrarCita {
     private JRadioButton radioButton2;
     private JRadioButton radioButton3;
     private JRadioButton radioButton4;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField diarioTextField;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JTextField textField12;
+    private JTextField textField13;
+    private JTextField textField14;
+    private JTextField textField15;
+    private JTextField textField16;
+    private JTextField textField17;
 }
