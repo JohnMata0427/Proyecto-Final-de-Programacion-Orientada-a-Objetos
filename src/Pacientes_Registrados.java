d3036035e3b499b6b637d8bb84c5cb3735dbfa1f
Dustin <dustin04ms@gmail.com>
2024-02-17 21:44:40 -0500

    ClientesRegistrados

---
+++ src/Pacientes_Registrados.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Pacientes_Registrados {
+    JPanel Pacientes;
+    private JLabel LogoRegistro;
+    private JLabel NombreUsuario;
+    private JButton cerrarSesiónButton;
+    private JTextField textField1;
+}
