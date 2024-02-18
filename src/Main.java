f1bdc0b634c77c3d093a66f52e78da52b31b2a3a
Alejandro <alejandro27campoverde@gmail.com>
2024-02-17 20:02:31 -0500

    RegistrarCita3

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setContentPane(new Login().Inicio_Sesion);
+        frame.setContentPane(new RegistrarCita().RegistrarCitaPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+        //frame.setSize(480,640);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setVisible(true);
 
--- src/RegistrarCita.java
+++ src/RegistrarCita.java
@@ -1,25 +1,7 @@
 import javax.swing.*;
 
 public class RegistrarCita {
-    private JRadioButton radioButton1;
-    private JRadioButton radioButton2;
-    private JRadioButton radioButton3;
-    private JRadioButton radioButton4;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
-    private JTextField diarioTextField;
-    private JTextField textField7;
-    private JTextField textField8;
-    private JTextField textField9;
-    private JTextField textField10;
-    private JTextField textField11;
-    private JTextField textField12;
-    private JTextField textField13;
-    private JTextField textField14;
-    private JTextField textField15;
-    private JTextField textField16;
-    private JTextField textField17;
+    public JPanel RegistrarCitaPanel;
+
+
 }
