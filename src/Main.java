c3e42ece12033dfac399a96e2d50a4d063582baf
jeseniaisabel231 <jesenia.pazto@epn.edu.ec>
2024-02-18 12:41:23 -0500

    modulos

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setContentPane(new Pacientes_Registrados().Pacientes);
+        frame.setContentPane(new Modulos().JPanelModulos);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         //frame.setSize(480,640);
---
+++ src/Modulos.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Modulos {
+    JPanel JPanelModulos;
+    private JLabel LogoRegistro;
+    private JLabel NombreUsuario;
+    private JButton cerrarSesiónButton;
+    private JButton verCitaButton;
+    private JButton registrarCitaButton;
+    private JButton resultadoDeExamenesButton;
+    private JButton historialMedicoButton;
+}
