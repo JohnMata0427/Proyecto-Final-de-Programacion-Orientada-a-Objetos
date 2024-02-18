81b0285ac11bc393939e7d736f52302705408cc4
Alejandro <alejandro27campoverde@gmail.com>
2024-02-17 19:30:41 -0500

    RegistroCita

---
+++ src/RegistrarCita.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class RegistrarCita {
+    private JRadioButton radioButton1;
+    private JRadioButton radioButton2;
+    private JRadioButton radioButton3;
+    private JRadioButton radioButton4;
+}
