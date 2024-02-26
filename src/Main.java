ada1aff062b5347aa198194a9094974e62f3743c
John Mata <john.mata@epn.edu.ec>
2024-02-25 23:39:13 -0500

    Mejoras x2

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ public class Main {
     static JFrame ventanaBase = new JFrame("MediCare");
     static JPanel loginPanel = new Login().panel;
     public static void main(String[] args) {
-        ventanaBase.setContentPane(loginPanel); // Inicio_Sesion es el JPanel que contiene la interfaz de inicio de sesion
+        ventanaBase.setContentPane(new Pacientes_Registrados().panel); // Inicio_Sesion es el JPanel que contiene la interfaz de inicio de sesion
         ventanaBase.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Al cerrar la ventana se termina el programa
         ventanaBase.setIconImage(new ImageIcon("src/imgs/Icon.png").getImage()); // Icono de la ventana
         ventanaBase.pack(); // Ajusta el tamaño de la ventana al tamaño del JPanel
-        ventanaBase.setSize(650,400); // Tamaño inicial de la ventana
+        ventanaBase.setSize(850,450); // Tamaño inicial de la ventana
         ventanaBase.setLocationRelativeTo(null); // Centra la ventana en la pantalla
         ventanaBase.setVisible(true); // Hace visible la ventana en pantalla
     }
--- src/Pacientes_Registrados.java
+++ src/Pacientes_Registrados.java
@@ -1,6 +1,8 @@
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 
@@ -11,23 +13,72 @@ public class Pacientes_Registrados {
     private JLabel correoLabel;
     private JButton cerrarSesionButton;
     private JTable pacientesTable;
-    private JTextArea textArea1;
-    private JTextArea textArea2;
-    private JTextArea textArea3;
+    private JButton actualizarTablaButton;
+    private JTextField buscarField;
+    private JButton buscarButton;
+    private JButton registrarButton;
+    private JComboBox sexoComboBox;
+    private JTextField fechaField;
+    private JEditorPane resultadosEditorPane;
+    private JTextArea medicamentoTextArea;
+    private JTextField historialMedicoField;
+    private JTextField horaField;
+    private JTextField imcField;
+    private JTextField nombreField;
+    private JTextField correoField;
+    private JTextField edadField;
+    private JTextField cedulaField;
+    private JTextField telefonoField;
+    private JTextField alergiasField;
+    private JTextField medicoField;
+    private JTextField motivoField;
+    private JTextField estaturaField;
+    private JTextField pesoField;
+    private JTextField temperaturaField;
+    private JTextField presionField;
+    private JTextArea tratamientoTextArea;
+    private JPanel formPanel;
 
     public Pacientes_Registrados() {
+        Border lineBorder = BorderFactory.createLineBorder(Color.decode("#7a8a99"), 1);
+
+        historialMedicoField.setBorder(lineBorder);
+        fechaField.setBorder(lineBorder);
+        horaField.setBorder(lineBorder);
+        imcField.setBorder(lineBorder);
+        resultadosEditorPane.setBorder(lineBorder);
+        medicamentoTextArea.setBorder(lineBorder);
+        tratamientoTextArea.setBorder(lineBorder);
+        nombreField.setBorder(lineBorder);
+        correoField.setBorder(lineBorder);
+        edadField.setBorder(lineBorder);
+        cedulaField.setBorder(lineBorder);
+        telefonoField.setBorder(lineBorder);
+        alergiasField.setBorder(lineBorder);
+        medicoField.setBorder(lineBorder);
+        motivoField.setBorder(lineBorder);
+        estaturaField.setBorder(lineBorder);
+        pesoField.setBorder(lineBorder);
+        temperaturaField.setBorder(lineBorder);
+        presionField.setBorder(lineBorder);
+
         usuarioLabel.setText(Login.usuario);
         correoLabel.setText(Login.correo);
+
         try {
             ResultSet resultado = Conexion.ejecutarQuery("SELECT * FROM Clientes");
             assert resultado != null;
             ResultSetMetaData metaData = resultado.getMetaData();
+
             int columnas = resultado.getMetaData().getColumnCount();
-            Object[] etiquetas = new Object[resultado.getMetaData().getColumnCount()];
+
+            Object[] etiquetas = new Object[columnas];
             for (int i=0; i<columnas; i++){
                 etiquetas[i] = metaData.getColumnLabel(i+1);
             }
+
             DefaultTableModel model = new DefaultTableModel(etiquetas, 0);
+
             while(resultado.next()){
                 Object[] filas = new Object[columnas];
                 for (int i=0; i<columnas; i++){
@@ -35,10 +86,12 @@ public class Pacientes_Registrados {
                 }
                 model.addRow(filas);
             }
+
             pacientesTable.setModel(model);
         } catch (Exception ex){
             System.out.println(ex);
         }
+
         cerrarSesionButton.addActionListener(e -> {
             Main.ventanaBase.setContentPane(Main.loginPanel);
             Main.ventanaBase.validate();
